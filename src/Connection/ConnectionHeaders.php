76b2376ccdba54c78ff7a8bb33716a47b5741cd1
Munna Khan <ahkmunna@gmail.com>
2020-03-27 19:58:38 +0600

    First commit

---
+++ src/Connection/ConnectionHeaders.php
@@ -0,0 +1,10 @@
+<?php
+namespace Haque\Cybersource\Connection;
+
+// require_once __DIR__ . DIRECTORY_SEPARATOR . '../../../Resources/autoload.php';
+
+interface ConnectionHeaders
+{
+    public function httpParseHeaders($raw_headers);
+    public function getConnection($signature, $resourcePath, $postData, $method, $merchantConfig);
+}
